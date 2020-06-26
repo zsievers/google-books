7b634ccc8a19f2c54d6a7ad9dd07a037161a648a
Zach <zjsievers@gmail.com>
2020-06-26 19:12:39 -0400

    fixed typos

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 // const routes = require("./routes");
-// const bodyParser = require('body-parser');
-
 const app = express();
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
-// DEFINE MIDDLEWARE
-app.use(express.urlencoded({ extended: true}));
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// serve up static assets (heroku)
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
-
-// add routes both api and view
+// Add routes, both API and view
 // app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {useNewUrlParser: true});
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googleBook");
 
 // Start the API server
 app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-  });
-
+  console.log("Listening on port http://localhost:" + PORT);
+});
