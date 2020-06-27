96caf8725eb94dfa1da75b4abecdab685b271261
Zach <zjsievers@gmail.com>
2020-06-27 13:39:19 -0400

    entered schema model

---
+++ client/src/utils/API.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default {
+    getSearchedBooks: function(query) {
+        return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
+    },
+    
+    saveBook: function(bookSaveChoice) {
+        return axios.post("/api/books", bookSaveChoice);
+    },
+
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+
+    getBook: function(id) {
+        return axios.get("/api/books/" + id);
+    },
+
+    deleteBook: function(id) {
+        return axios.delete("/api/books/" + id);
+    }
+};
