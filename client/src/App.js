145535cb954ec875e90b9fe65f7f41bda12a6f00
Zach <zjsievers@gmail.com>
2020-06-28 22:46:23 -0400

    importing Nav into return ()

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import Nav from "./components/Nav";
-import NoMatch from "./pages/NoMatch";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 
@@ -11,6 +10,7 @@ function App() {
     <div className="App">
         <Router>
           <div>
+            <Nav/>
             <Switch>
               <Route exact path ="/" component={Search}/>
               <Route exact path ="/books" component={Search}/>
