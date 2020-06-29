df8b8c69c0d56e5edee69b4766e4748d15b77e16
Zach <zjsievers@gmail.com>
2020-06-28 22:53:56 -0400

    tweaking card and styling

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "../Grid";
-import "./style.css";
+import './style.css'
 
-const Card = (props) => {
+const Card = props => {
   return props.books.length === 0 ? (
     <div />
   ) : (
     <div>
-      {props.books.map((book) => {
+      {props.books.map(book => {
         return (
           <Container fluid>
             <Row>
@@ -20,25 +20,24 @@ const Card = (props) => {
             <Row>
               <Col size="md-12">
                 <a className="link" href={book.link}>
-                  {book.link}
+                  {book.link}>
                 </a>
               </Col>
             </Row>
+
             <Row className="mb-4">
               <Col size="sm-1" className="pr-0">
                 <img className="bookimg" src={book.image} alt="" />
               </Col>
               <Col size="md-7">
-                <h6 className="author">
-                  {book.author} - {book.date}
-                </h6>
+                <h6 className="author">{book.author} - {book.date}</h6>
                 <p className="description">{book.description}</p>
               </Col>
               <Col size="md-1">
                 <button
-                  className="btn button"
+                  className=" btn button"
                   id={book.id}
-                  onClick={(event) => props.handleSavedButton(event)}
+                  onClick={event => props.handleSavedButton(event)}
                 >
                   Save
                 </button>
